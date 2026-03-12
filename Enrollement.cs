71d6af98422532906b6cd800cb5b4455bd2745f5
janshivemula <janshivemula@gmail.com>
2026-03-12 20:29:11 +0530

    Add files via upload

---
+++ Enrollement.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UniversityEnrollement
+{
+    internal class Enrollement
+    {
+        static void Main(string[] args)
+        {
+            OnlineCourse c1 = new OnlineCourse();
+            InPersonCourse c2 = new InPersonCourse();
+            LabCourse c3 = new LabCourse();
+
+            c1.coursename = ".Net";
+            c1.Enroll("Rama");
+
+            c2.coursename = "DataBase";
+            c2.Enroll("Janaki");
+
+            c3.coursename = "ReactJS";
+            c3.Enroll("SitaRama");
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+
+namespace UniversityEnrollement
+{
+    class Course
+    {
+        public string coursename;
+          
+
+    }
+    class OnlineCourse : Course
+    {
+        public void Enroll(string studentname)
+        {
+            if (studentname != " ")
+                Console.WriteLine(studentname + "Enrolled in Online Course : " + coursename);
+            else
+                Console.WriteLine("Invalid student name");
+        }
+    }
+    class InPersonCourse : Course
+    {
+        public void Enroll(string studentname)
+        {
+            if (studentname != " ")
+                Console.WriteLine(studentname + "Enrolled in In - Person Course : " + coursename);
+            else
+                Console.WriteLine("Invalid student name");
+        }
+    }
+    class LabCourse : Course
+    {
+        public void Enroll(string studentname)
+        {
+            if (studentname != " ")
+                Console.WriteLine(studentname + "Enrolled in Lab Course : " + coursename);
+            else
+                Console.WriteLine("Invalid student name");
+        }
+    }
+}
