d8970550c86642a829ea8470ff00e9fda2df8a4e
janshivemula <janshivemula@gmail.com>
2026-03-18 21:17:43 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,52 +1,16 @@
-﻿namespace InOut
+﻿namespace Boxing_Unboxing
 {
-    class InOut
+    class BodyMass
     {
-        static void ShowResult(in int num, out int result )
-        {
-            result = num * num;
-        }
-        static int Sum(params int[] numbers)
-        {
-            int sum = 0;
-            for(int i =0; i< numbers.Length; i++)
-            {
-                sum = sum + numbers[i];
-            }
-            return sum;
-        }
         static void Main(string[] args)
         {
-            Console.WriteLine("POC on In and Out Variables");
-            int number = 5;
-            int result;
-            ShowResult(number, out result);
-            Console.WriteLine("Number : " + number);
-            Console.WriteLine("Result : " + result);
-            Console.WriteLine();
+            int weight = 38;
+            object objweight = weight;
+            Console.WriteLine("Boxed Weight: {0}", objweight);
 
-            Console.WriteLine("POC on Params");
-            int total = Sum(12, 05, 18, 10);
-            Console.WriteLine("Sum : " + total);
-            Console.WriteLine();
-
-            Console.WriteLine("POC on Integer.TryParse()");
-            string input = "123";
-            int value;
-            if(int.TryParse(input , out value))
-            {
-                Console.WriteLine("Conversion Successful : " + value);
-            }
-            else
-            {
-                Console.WriteLine("Invalid input given, give proper input");
-            }
-            Console.WriteLine();
-
-            Console.WriteLine("WriteLine using params");
-            Console.WriteLine("Name : {0}, Age : {1}", "Janaki", 23);
-
+            int unboxedweight = (int)objweight;
+            Console.WriteLine("UnBoxed Weight: {0}", unboxedweight);
+            Console.ReadLine();
         }
-        
     }
 }
